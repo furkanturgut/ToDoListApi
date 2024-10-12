fe718e2e7e25dc99c09492ba3cb48ef95c07e7bb
furkant <fturgut1126@gmail.com>
2024-10-12 16:50:01 +0300

    Create Models

---
+++ TodoListApi/Models/TodoStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoListApi.Models
+{
+    public class TodoStatus
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<TodoTask> Tasks { get; set; }
+    }
+}
---
+++ TodoListApi/Models/TodoTask.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoListApi.Models
+{
+    public class TodoTask
+    {
+        public int Id { get; set; }
+        public string Task { get; set; }
+        public User User { get; set; }
+        public TodoStatus TodoStatus { get; set; }
+    }
+}
