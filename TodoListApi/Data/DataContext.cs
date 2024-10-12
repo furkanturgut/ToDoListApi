f284efd71953cb9a7b8a528d8e468a5f2e9be991
furkant <fturgut1126@gmail.com>
2024-10-12 18:15:56 +0300

    Create Data Context

---
+++ TodoListApi/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoListApi.Models;
+
+namespace TodoListApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options ) :base(options)
+        {
+
+        }
+        public DbSet<TodoStatus> todoStatuses { get; set; }
+        public DbSet<User> users { get; set; }
+        public DbSet<TodoTask> todoTasks{ get; set; }
+
+
+    }
+}
